45213ad4eb92d9db7f5802b3f6a66ff3071a7ae5
Dodo12w <79851069+Dodo12w@users.noreply.github.com>
2021-03-06 23:54:02 -0500

    Testing references

---
+++ src/Client/Controllers/TestReferencesController.lua
@@ -0,0 +1,20 @@
+-- Test References Controller
+-- Username
+-- March 6, 2021
+
+
+
+local TestReferencesController = {}
+
+function TestReferencesController:Start()
+    
+ 
+end
+
+
+function TestReferencesController:Init()
+	
+end
+
+
+return TestReferencesController
--- src/Server/Services/FirstService.lua
+++ src/Server/Services/FirstService.lua
@@ -11,10 +11,7 @@ function FirstService:SayHello()
 end
 
 function FirstService:Start()
-	self.Services.AnotherService:ConnectEvent("Hello", function(message)
-        print("We recieved a message: " .. message)
-        print("We should see this inside of the github soon!!! HOPEFULLY!!!!!")
-    end)
+	
 end
 
 
