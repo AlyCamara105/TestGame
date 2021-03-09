77251759732d347fced7bfa9de550b2f76cec9a8
Dodo12w <79851069+Dodo12w@users.noreply.github.com>
2021-03-09 17:57:21 -0500

    Second commit today

--- src/Server/Services/FirstService.lua
+++ src/Server/Services/FirstService.lua
@@ -12,6 +12,9 @@ end
 
 function FirstService:Start()
 	print("This local script is fired from the client side")
+	print("I actually decided to test out something, and it works so I will merge it with the master branch.")
+	print("I added the new changes into master branch. But I haven't commited it to the remote repo yet....")
+	print("The changes are also on the second branch ....")
 end
 
 
